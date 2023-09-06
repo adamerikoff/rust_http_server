e5a2ddd3936d7438d46c3f3c1facea920da3de01
adamerikoff <adamerikoff@protonmail.com>
2023-09-06 07:25:41 +0600

    RustHttpServer struct and impl added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let r_server = RustHttpServer::new("127.0.0.1:1995");
+    r_server.run();
 }
+
+struct RustHttpServer {
+    address: String,
+}
+
+impl RustHttpServer {
+    fn new(address: String) -> Self{
+        Self {
+            address: address
+        }
+    }
+
+    fn run(self) {
+        
+    }
+}
+
