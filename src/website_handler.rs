408852a9725be8c12f84426f20855ba563043ac0
adamerikoff <adamerikoff@protonmail.com>
2023-09-07 03:45:20 +0600

    GETTERS ADDED

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,14 +1,25 @@
-use crate::http::{request_module::Request, response_module::Response};
+use crate::http::{
+    request_module::Request, 
+    response_module::Response,
+    status_code_module::StatusCode,
+    method_module::Method
+};
 
 use super::rust_http_server_module::Handler;
 
 pub struct WebsiteHandler;
 
+
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(
-            crate::http::status_code_module::StatusCode::Ok,
-            Some("TEST".to_string())
-        )
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("ROOT".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("HELLO WORLD".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+
+        }
     }
 }
